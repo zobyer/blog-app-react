6d79139d3f37fcd607b702cb2a966867c9959c51
zobyer-welldev <ahmzobyer@gmail.com>
2023-03-14 15:01:42 +0600

    feat: blog card component added

--- src/app/components/card/blog/index.tsx
+++ src/app/components/card/blog/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Styles from "./index.module.scss";
 
 const BlogCard = () => {
-  return <div>Blog Card</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${require("assets/images/banner-item-01.jpg")})`,
+        backgroundSize: "cover",
+      }}
+      className={Styles.blogCard}
+    >
+      <div className={Styles.infoContainer}>
+        <h6 className={Styles.heading}>Fashion</h6>
+        <h4 className={Styles.title}>Morbi dapibus condimentum</h4>
+        <p className={Styles.othersInfo}>
+          <span>Admin</span>
+          <span>May 12, 2022</span>
+          <span>comments</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogCard;
