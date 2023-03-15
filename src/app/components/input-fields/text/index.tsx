f890cf5a13ef2a79d230c2d9dbc427a9873c5b91
zobyer-welldev <ahmzobyer@gmail.com>
2023-03-15 13:34:53 +0600

    feat: input field component init

--- src/app/components/input-fields/text/index.tsx
+++ src/app/components/input-fields/text/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import Styles from "./index.module.scss";
 
-const TextInputFields: React.FC = () => {
+import Styles from "./index.module.scss";
+import { TextInputField as TextInputFieldProps } from "./index.type";
+
+const TextInputFields: React.FC<TextInputFieldProps> = (props) => {
+  const {
+    placeholder = "type to search",
+    styledClassName = "",
+    isLabelVisible = false,
+  } = props;
+
   return (
     <>
-      <input type="text" className={Styles.textInput} />
+      {isLabelVisible && <label htmlFor="textInputField">Enter</label>}
+      <input
+        type="text"
+        id="textInputField"
+        className={`${styledClassName} ${Styles.textInput}`}
+        placeholder={placeholder}
+      />
     </>
   );
 };
